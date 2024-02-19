186a21ba49f36a842cf869e38a8d9b683c7db2e3
Deepankar Pant <91065406+deepankarpant15@users.noreply.github.com>
2024-02-19 15:04:40 +0530

    exercises

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -119,4 +119,156 @@ const [a, b, ...other] = [1, 2, 345, 5, 6, 7, 8];
 console.log(a, b, other);
 
 // for functions rest paratmeters
+
+
+////////////////////////////////////////////
+//  Optional chaining in JS (obejects)
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+//----- for of loop
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+// " ?? -> nullish operator feature in js for defining undefined & NULL values"
+// ----- IN Mrthods
+
+console.log(restaurant.order?.(2, 0) ?? 'Method doesnt exist');
+console.log(restaurant.starterMeu?.(2, 0) ?? 'Method doesnt exist');
+
+// -------- Ch Object Looping
+//----- .1 Propert Names
+
+const properties = Object.keys(restaurant.openingHours);
+console.log(properties);
+
+let openstr = `We are open at ${properties.length} days: `;
+for (const day of properties) {
+  openstr += `${day},`;
+}
+console.log(openstr);
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// ------- Ch Sets
+const staff = ['waiter', 'Cheif', 'Manager', 'Waiter ', 'Cheif'];
+
+const satffunique = [...new Set(staff)];
+console.log(satffunique);
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//------------ // Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+//---------- Maps: Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+///////////////////////////////////////
+
+//------------- Working With Strings - Part 1
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat 😬');
+  else console.log('You got lucky 😎');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('jonas'));  // conevrting string primitive into string object
+console.log(typeof new String('jonas'));
+
+console.log(typeof new String('jonas').slice(1));
 */
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate'));
